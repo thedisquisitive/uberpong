5f487db1792c09e94d12346edf54c2280d679920
thedisquisitive <zach@thedisquisitive.com>
2017-08-13 14:34:32 -0400

    Initial commit

---
+++ Source/uberpong/uberpong.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "uberpong.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, uberpong, "uberpong" );
